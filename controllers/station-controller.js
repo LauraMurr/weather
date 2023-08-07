8a339e447b8a6494812a3bcd9f5e9bd698d9e1fb
Laura Murray <20104312@mail.wit.ie>
2023-08-07 19:28:54 +0100

    Push after fixing issue where readings were not being displayed and sampled some css

---
+++ controllers/station-controller.js
@@ -0,0 +1,28 @@
+import { stationStore } from "../models/station-store.js";
+import { readingStore } from "../models/reading-store.js";
+
+export const stationController = {
+  async index(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const viewData = {
+      title: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+
+  async addReading(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReading = {
+      code: Number(request.body.code),
+      temp: Number(request.body.temp),
+      windspeed: Number(request.body.windspeed),
+    };
+    console.log(`adding reading ${newReading.code}, ${newReading.temp}, ${newReading.windspeed},`);
+    
+    console.log("Station Readings:", station.readings);
+    
+    await readingStore.addReading(station._id, newReading);
+    response.redirect("/station/" + station._id);
+  },
+};
